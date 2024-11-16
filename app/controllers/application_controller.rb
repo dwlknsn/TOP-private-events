97005eb122eecea2f91bdb72578f044d4e3b1e4e
dwlknsn <dwlknsn@users.noreply.github.com>
2024-11-16 12:08:42 +0700

    Add Events resource and link to User model as 'host' <-> 'hosted_events'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_user
+
+  private
+
+  def set_user
+    @user ||= current_user || User.first
+  end
 end
