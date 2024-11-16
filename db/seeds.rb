6045c3771fc0e670273d8bae742f2b51b134ce27
dwlknsn <dwlknsn@users.noreply.github.com>
2024-11-16 13:40:10 +0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+u1 = User.create!(email: "a@a.com", password: "password")
+u2 = User.create!(email: "b@b.com", password: "password")
+u3 = User.create!(email: "c@c.com", password: "password")
+
+2.times do |i|
+  u1.hosted_events.create(
+    name: "#{u1.email}'s Event #{i + 1}",
+    description: "Event #{i + 1} description",
+    location: "#{u1.email}'s place",
+    datetime: Time.now + i.hours
+  )
+end
+
+2.times do |i|
+  u2.hosted_events.create(
+    name: "#{u2.email}'s Event #{i + 1}",
+    description: "Event #{i + 1} description",
+    location: "#{u2.email}'s place",
+    datetime: Time.now + i.hours
+  )
+end
+
+u1.sign_ups.create(event: u2.hosted_events.first)
+u2.sign_ups.create(event: u1.hosted_events.first)
+u3.sign_ups.create(event: u1.hosted_events.first)
+u3.sign_ups.create(event: u1.hosted_events.last)
+u3.sign_ups.create(event: u2.hosted_events.first)
+u3.sign_ups.create(event: u2.hosted_events.last)
